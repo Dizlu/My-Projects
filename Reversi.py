000b752812028519f8717a5bdbca0cc8d06239c7
Marcin Gol <golmarcin@gmail.com>
2014-09-07 15:13:31 +0200

    Another lazy commit...

--- Reversi.py
+++ Reversi.py
@@ -145,7 +145,7 @@ def getPlayerMove(board, playerTile):
 	DIGITS1TO8 = '1 2 3 4 5 6 7 8'.split()
 
 	while True:
-		move = raw_input('Enter your move or type quir to end the game, or hints to turn off/ on hints! ').lower()
+		move = raw_input('Enter your move or type quit to end the game, or hints to turn off/ on hints! ').lower()
 		if move == 'quit':
 			return 'quit'
 		if move == 'hints':
@@ -158,5 +158,25 @@ def getPlayerMove(board, playerTile):
 				continue
 			else:
 				break
+		else:
+			print('That is not a valid move. Type the x digit (1- 8), then the y digit (1-8)')
+			print('For example, 81 will be the top- right corner')
 
-		
+	return [x, y]
+
+def getComputerMove(board, computerTile):
+	possibleMoves = getValidMoves(board, computerTile)
+	random.shuffle(possibleMoves)
+
+	for x, y in possibleMoves:
+		if isOnCorner(x, y):
+			return [x, y]
+		bestScore = -1
+		for x, y in possibleMoves:
+			dupeBoard = getBoardCopy(board)
+			makeMove(dupeBoard, computerTile,x , y)
+			score = getSCoreOfBoard(dupeBoard)[computerTile]
+			if score > bestScore:
+				bestMove = [x, y]
+				bestScore = score
+		return bestMove
